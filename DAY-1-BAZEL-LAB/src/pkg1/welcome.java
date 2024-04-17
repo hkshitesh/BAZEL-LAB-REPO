6c0b6ff84268aea9764a98a5604b0931d5339bac
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-17 16:52:35 +0530

    pkg2 added

---
+++ DAY-1-BAZEL-LAB/src/pkg1/welcome.java
@@ -0,0 +1,8 @@
+package pkg1;
+import pkg2.Greeting;
+public class welcome {
+    public static void main(String[] args) {
+       Greeting.sayHi(); 
+    }
+    
+}
---
+++ DAY-1-BAZEL-LAB/src/pkg2/Greeting.java
@@ -0,0 +1,7 @@
+package pkg2;
+public class Greeting {
+    public static void sayHi()
+    {
+        System.out.println("Hi Bazel");
+    }
+}
